98bea20750a72c2f8ef9b97061c7cae1bb4b1600
estebans-tech <estebanstech@gmail.com>
2026-03-27 10:12:09 +0100

    feat: add PageContainer component

---
+++ src/lib/components/PageContainer.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  import type { Snippet } from 'svelte'
+
+  const { children, class: className = '' } = $props<{ children: Snippetm, class?: string }>()
+</script>
+
+<div class="mx-auto w-full max-w-screen-sm px-4 md:max-w-2xl md:px-6 lg:max-w-3xl {className}">
+  {@render children()}
+</div>
